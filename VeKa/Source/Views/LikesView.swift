266e6e9a9321c97d180fef899ed63e09743d10eb
slipStas <41761920+slipStas@users.noreply.github.com>
2019-09-23 10:07:37 +0300

    add count likes label color change

--- VeKa/Source/Views/LikesView.swift
+++ VeKa/Source/Views/LikesView.swift
@@ -20,14 +20,16 @@ class LikesView: UIView {
             switch likesStatus {
             case .like:
                 likeIcon.image = UIImage(named: "heart_fill")
+                label.textColor = UIColor.red
             case .noLike:
                 likeIcon.image = UIImage(named: "heart")
+                label.textColor = UIColor.darkText
             }
         }
     }
-    let likeIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    let likeIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
 
-    let label = UILabel(frame: CGRect(x: 44, y: 0, width: 100, height: 44))
+    let label = UILabel(frame: CGRect(x: 30, y: 0, width: (30 * 3), height: 30))
     
     
     var onTap: (() -> Void)?
@@ -39,6 +41,7 @@ class LikesView: UIView {
         addSubview(likeIcon)
         
         likeIcon.image = UIImage(named: "heart")
+        label.textColor = UIColor.darkText
         
         let gr = UITapGestureRecognizer(target: self, action: #selector(tapOnLike(guestRecogniser:)))
         addGestureRecognizer(gr)
