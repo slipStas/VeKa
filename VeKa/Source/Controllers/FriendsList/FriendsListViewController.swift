55d8b26c55fca7fa6625ad7187b9e9874b120988
slipStas <41761920+slipStas@users.noreply.github.com>
2019-09-26 04:22:18 +0300

    add sectionIndexTitles

--- VeKa/Source/Controllers/FriendsList/FriendsListViewController.swift
+++ VeKa/Source/Controllers/FriendsList/FriendsListViewController.swift
@@ -16,24 +16,27 @@ class FriendsListViewController: UIViewController {
         User(name: "Elena", avatar: (UIImage(named: "image_3")!), likesCount: 7653, likesStatus: .noLike),
         User(name: "Oleg", avatar: (UIImage(named: "image_4")!), likesCount: 9719, likesStatus: .noLike),
         User(name: "Mikhail", avatar: (UIImage(named: "image_5")!), likesCount: 54, likesStatus: .noLike),
-        User(name: "Valera1", avatar: (UIImage(named: "image_1")!), likesCount: 567, likesStatus: .noLike),
-        User(name: "Igor1", avatar: (UIImage(named: "image_2")!), likesCount: 4568, likesStatus: .noLike),
-        User(name: "Elena1", avatar: (UIImage(named: "image_3")!), likesCount: 1, likesStatus: .noLike),
-        User(name: "Oleg1", avatar: (UIImage(named: "image_4")!), likesCount: 423, likesStatus: .noLike),
-        User(name: "Mikhail1", avatar: (UIImage(named: "image_5")!), likesCount: 974, likesStatus: .noLike),
-        User(name: "Valera2", avatar: (UIImage(named: "image_1")!), likesCount: 1113, likesStatus: .noLike),
-        User(name: "Igor2", avatar: (UIImage(named: "image_2")!), likesCount: 96528, likesStatus: .noLike),
-        User(name: "Elena2", avatar: (UIImage(named: "image_3")!), likesCount: 100000, likesStatus: .noLike),
-        User(name: "Oleg2", avatar: (UIImage(named: "image_4")!), likesCount: 2, likesStatus: .noLike),
-        User(name: "Mikhail2", avatar: (UIImage(named: "image_5")!), likesCount: 89, likesStatus: .noLike)
+        User(name: "John", avatar: (UIImage(named: "image_1")!), likesCount: 567, likesStatus: .noLike),
+        User(name: "Stanislav", avatar: (UIImage(named: "image_2")!), likesCount: 4568, likesStatus: .noLike),
+        User(name: "Olga", avatar: (UIImage(named: "image_3")!), likesCount: 1, likesStatus: .noLike),
+        User(name: "Viktor", avatar: (UIImage(named: "image_4")!), likesCount: 423, likesStatus: .noLike),
+        User(name: "Denis", avatar: (UIImage(named: "image_5")!), likesCount: 974, likesStatus: .noLike),
+        User(name: "Alex", avatar: (UIImage(named: "image_1")!), likesCount: 1113, likesStatus: .noLike),
+        User(name: "Alexander", avatar: (UIImage(named: "image_2")!), likesCount: 96528, likesStatus: .noLike),
+        User(name: "Julia", avatar: (UIImage(named: "image_3")!), likesCount: 100000, likesStatus: .noLike),
+        User(name: "Pavel", avatar: (UIImage(named: "image_4")!), likesCount: 2, likesStatus: .noLike),
+        User(name: "Anton", avatar: (UIImage(named: "image_5")!), likesCount: 89, likesStatus: .noLike)
     ]
     
-    var filterdFriendsArray: [User] = []
-
+    var friendsFiltered: [User] = []
+    
+    var sections: [String] = []
+    var friendsInSections: [String: [User]] = [:]
+    
+    private var selectedFriend: User?
+    
     @IBOutlet weak var friendsListTableView: UITableView!
-    
-    @IBOutlet weak var filterFriendsControl: FilterFriendsView!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,41 +45,48 @@ class FriendsListViewController: UIViewController {
         friendsListTableView.dataSource = self
         friendsListTableView.delegate = self
         
-        filterdFriendsArray = filterArray()
+        self.friendsFiltered = self.friendsArray
         
-        filterFriendsControl.charracterArray = charArray()
-        print(filterFriendsControl.charracterArray)
-        
-        filterFriendsControl.addView()
-        filterFriendsControl.addCharViews()
-        filterFriendsControl.onScroll = {
-            print(self.filterFriendsControl.filterView.arrangedSubviews.endIndex)
-        }
+        fillSections()
+        fillSectionsWithFriends()
     }
 }
 
 extension FriendsListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("""
-            select friend "\(filterdFriendsArray[indexPath.row].name)"
+        let sectionName: String = self.sections[indexPath.section]
+        if let friendsInSection: [User] = self.friendsInSections[sectionName] {
+            print("""
+            select friend "\(friendsInSection[indexPath.row].name)"
             """)
+        }
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
 extension FriendsListViewController: UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendsArray.count
+        let sectionName: String = self.sections[section]
+        if let friendsInSection: [User] = self.friendsInSections[sectionName] { return friendsInSection.count }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = friendsListTableView.dequeueReusableCell(withIdentifier: "friendsListIdentifire", for: indexPath) as! FriendsListTableViewCell
         
-        cell.friendNameLabel.text = filterdFriendsArray[indexPath.row].name
-        cell.friendsPhotoImageView.image = filterdFriendsArray[indexPath.row].avatar
         
+        let sectionName: String = self.sections[indexPath.section]
+        if let friendsInSection: [User] = self.friendsInSections[sectionName] {
+            cell.friendNameLabel.text = friendsInSection[indexPath.row].name
+            cell.friendsPhotoImageView.image = friendsInSection[indexPath.row].avatar
+        }
         
         return cell
     }
@@ -87,24 +97,50 @@ extension FriendsListViewController: UITableViewDataSource {
             if let indexPath = friendsListTableView.indexPathForSelectedRow {
                 if let destinationVC = segue.destination  as? FriendInfoViewController {
                     
-                    destinationVC.friendInfoList = [(filterdFriendsArray[(indexPath as NSIndexPath).row])]
+                    destinationVC.friendInfoList = [(friendsArray[(indexPath as NSIndexPath).row])]
                 }
             }
         }
     }
     
-    func filterArray() -> [User] {
-        return friendsArray.sorted(by: {$0.name.first! < $1.name.first!})
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sections
     }
     
-    func charArray() -> [String] {
-        var array : [String] = []
-        for i in filterdFriendsArray {
-            if !array.contains(String(i.name.first!)) {
-                array.append(String(i.name.first!))
-            }
+    func filter(query: String) {
+        friendsFiltered.removeAll()
+        
+        var isInFilter = true
+        
+        for friend in friendsArray {
+            if query.count > 0 { isInFilter = (friend.name.lowercased().contains(query.lowercased())) }
+            if isInFilter { friendsFiltered.append(friend) }
         }
-        return array
+        
+        fillSections()
+        fillSectionsWithFriends()
     }
     
+    func fillSections() {
+        sections = Array(Set(friendsFiltered.map { String(($0.name.first)!) })).sorted()
+    }
+    
+    func fillSectionsWithFriends() {
+        friendsInSections.removeAll()
+        
+        for friend in friendsFiltered {
+            guard let firstLetter = friend.name.first else { continue }
+            
+            var friends: [User] = []
+            
+            if let friendsInSection = friendsInSections[String(firstLetter)] {
+                friends.append(contentsOf: friendsInSection)
+            }
+            
+            friends.append(friend)
+            
+            friendsInSections[String(firstLetter)] = friends
+        }
+    }
 }
+
