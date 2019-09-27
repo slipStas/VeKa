328513434ffb1ca692126f1f4bee693475e15aa3
slipStas <41761920+slipStas@users.noreply.github.com>
2019-09-27 05:52:16 +0300

    add search bar in all groups controller

--- VeKa/Source/Controllers/AllGroupsList/AllGroupsViewController.swift
+++ VeKa/Source/Controllers/AllGroupsList/AllGroupsViewController.swift
@@ -10,44 +10,80 @@ import UIKit
 
 class AllGroupsViewController: UIViewController {
     
-    let allGroupsArray = [
-        Group(name: "AppStores", icon: UIImage(named: "icon_1")!),
-        Group(name: "Safaries", icon: UIImage(named: "icon_2")!),
-        Group(name: "Mails", icon: UIImage(named: "icon_3")!),
-        Group(name: "ApleTVs", icon: UIImage(named: "icon_4")!),
-        Group(name: "Homes", icon: UIImage(named: "icon_5")!),
-        Group(name: "Siries", icon: UIImage(named: "icon_6")!),
-        Group(name: "Locators", icon: UIImage(named: "icon_7")!),
-        Group(name: "Photo Boothes", icon: UIImage(named: "icon_8")!),
-        Group(name: "Shares", icon: UIImage(named: "icon_9")!),
-        Group(name: "Maps", icon: UIImage(named: "icon_10")!),
-    ]
+    var allGroupsArray: [Group] = []
+    var filteredGroups: [Group] = []
 
     @IBOutlet weak var allGroupsTableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         allGroupsTableView.rowHeight = 66
         allGroupsTableView.dataSource = self
+        
+        setUpSearchBar()
+        setUpGroups()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         //animateHeightTable(tableView: allGroupsTableView)
     }
+    
+    private func setUpGroups() {
+        allGroupsArray.append(Group(name: "AppStores", icon: UIImage(named: "icon_1")!))
+        allGroupsArray.append(Group(name: "Safaries", icon: UIImage(named: "icon_2")!))
+        allGroupsArray.append(Group(name: "Mails", icon: UIImage(named: "icon_3")!))
+        allGroupsArray.append(Group(name: "ApleTVs", icon: UIImage(named: "icon_4")!))
+        allGroupsArray.append(Group(name: "Homes", icon: UIImage(named: "icon_5")!))
+        allGroupsArray.append(Group(name: "Siries", icon: UIImage(named: "icon_6")!))
+        allGroupsArray.append(Group(name: "Locators", icon: UIImage(named: "icon_7")!))
+        allGroupsArray.append(Group(name: "Photo Boothes", icon: UIImage(named: "icon_8")!))
+        allGroupsArray.append(Group(name: "Shares", icon: UIImage(named: "icon_9")!))
+        allGroupsArray.append(Group(name: "Maps", icon: UIImage(named: "icon_10")!))
+        
+        filteredGroups = allGroupsArray
+    }
+    
+    private func setUpSearchBar() {
+        searchBar.delegate = self
+
+    }
+}
+
+extension AllGroupsViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            filteredGroups = allGroupsArray
+            allGroupsTableView.reloadData()
+            return
+        }
+        filteredGroups = allGroupsArray.filter({$0.name.contains(searchText)})
+        
+        allGroupsTableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
+    
+    
 }
 
 extension AllGroupsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allGroupsArray.count
+        return filteredGroups.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = allGroupsTableView.dequeueReusableCell(withIdentifier: "allGroupsIdentifier", for: indexPath) as! AllGroupsTableViewCell
         
-        cell.allGroupImageView.image = allGroupsArray[indexPath.row].icon
-        cell.allGroupLabel.text = allGroupsArray[indexPath.row].name
+        cell.allGroupImageView.image = filteredGroups[indexPath.row].icon
+        cell.allGroupLabel.text = filteredGroups[indexPath.row].name
         
         return cell
     }
