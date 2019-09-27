6f1500cf469d7ca80c6ab2897371048e1b434c31
slipStas <41761920+slipStas@users.noreply.github.com>
2019-09-28 02:06:47 +0300

    111

---
+++ VeKa/Source/Controllers/News/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 28.09.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VeKa/Source/Controllers/News/NewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsViewController.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 28.09.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
