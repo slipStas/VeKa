ba494cfa655f14bab2cb3893707f0f7703c9afed
slipStas <41761920+slipStas@users.noreply.github.com>
2019-10-02 13:19:38 +0300

    add animate to friend icon

--- VeKa/Source/Controllers/FriendInfo/FriendInfoCollectionViewCell.swift
+++ VeKa/Source/Controllers/FriendInfo/FriendInfoCollectionViewCell.swift
@@ -10,17 +10,43 @@ import UIKit
 
 class FriendInfoCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var animateView: UIView!
+    
     @IBOutlet weak var friendInfoImageView: UIImageView!
     
     @IBOutlet weak var nameFriendInfoLabel: UILabel!
     
     @IBOutlet weak var likesView: LikesView!
     
+    
+    
+    override func awakeFromNib() {
+        
+        let tapOnIcon = UITapGestureRecognizer(target: self, action: #selector(tapOnIconView))
+        tapOnIcon.numberOfTapsRequired = 1
+        animateView.addGestureRecognizer(tapOnIcon)
+    }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         
         friendInfoImageView.image = nil
         nameFriendInfoLabel.text = nil
+    }
+    
+    @objc func tapOnIconView(sender: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0,
+                       options: [],
+                       animations: {
+                        self.friendInfoImageView.transform = CGAffineTransform.identity.scaledBy(x: 1.4, y: 1.4)
+                        self.friendInfoImageView.transform = CGAffineTransform.identity.scaledBy(x: 0.6, y: 0.6)
+                        self.friendInfoImageView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
+
+                        print("tap on image")
+        })
     }
     
 }
