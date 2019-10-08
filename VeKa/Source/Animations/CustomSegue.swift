14d889018106dfbb8b2c7fc71d00fb2d45a7a59f
slipStas <41761920+slipStas@users.noreply.github.com>
2019-10-08 05:27:46 +0300

    add custom segue on login controller

---
+++ VeKa/Source/Animations/CustomSegue.swift
@@ -0,0 +1,32 @@
+//
+//  CustomSegue.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 08.10.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomSegue: UIStoryboardSegue {
+
+    let duration: TimeInterval = 1
+    
+    override func perform() {
+        
+        let width = source.view.bounds.width
+        let height = source.view.frame.height
+        
+        guard let containerView = source.view else {return}
+        containerView.addSubview(destination.view)
+        source.view.frame = containerView.frame
+        destination.view.frame = containerView.frame
+        destination.view.transform = CGAffineTransform(translationX: width, y: height)
+        
+        UIView.animate(withDuration: duration, animations: {
+            self.destination.view.transform = .identity
+        }, completion: { _ in
+            self.source.present(self.destination, animated:  false)})
+    }
+    
+}
