422e6a94b41769a46824ce952f3013a04316ba24
slipStas <41761920+slipStas@users.noreply.github.com>
2019-12-07 13:54:45 +0300

    add controller with friends from server

---
+++ VeKa/Source/Controllers/OnlineFriendsList/OnlineFriendsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OnlineFriendsListTableViewCell.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 07.12.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class OnlineFriendsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VeKa/Source/Model/ServerUsers.swift
@@ -0,0 +1,9 @@
+//
+//  ServerUsers.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 07.12.2019.
+//  Copyright © 2019 Stanislav Slipchenko. All rights reserved.
+//
+
+import Foundation
