ee1e85b350b5392e49b5c78f2488937564948372
slipStas <41761920+slipStas@users.noreply.github.com>
2019-12-10 20:53:56 +0300

    add avatars in tableView

--- VeKa/Source/Controllers/OnlineFriendsList/OnlineFriendsListViewController.swift
+++ VeKa/Source/Controllers/OnlineFriendsList/OnlineFriendsListViewController.swift
@@ -45,7 +45,11 @@ extension OnlineFriendsListViewController: UITableViewDataSource {
         let cell = onlineFriendsListTable.dequeueReusableCell(withIdentifier: "onlineFriendsList", for: indexPath) as! OnlineFriendsListTableViewCell
         
         cell.friendNameLabel.text = (vkApi.serverFriendList?.response.items[indexPath.row].firstName ?? "no name") + " " + (vkApi.serverFriendList?.response.items[indexPath.row].lastName ?? "no last name")
-        cell.friendIcon.image = UIImage(named: "image_2")
+        
+        if let url = NSURL(string: (vkApi.serverFriendList?.response.items[indexPath.row].photo200_Orig) ?? "https://vk.com/images/camera_200.png?ava=1") {
+            let data = NSData(contentsOf: url as URL)
+            cell.friendIcon.image = UIImage(data: data! as Data)
+        }
         
         self.onlineFriendsListTable.rowHeight = cell.friendIcon.frame.height + CGFloat(16)
 
